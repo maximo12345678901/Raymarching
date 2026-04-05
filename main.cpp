f10e859ce87cbc86e69297b82109a1ca55fcd345
maximo12345678901 <maximozweers@gmail.com>
2026-04-05 11:43:05 +0200

    Simple rendering loop

--- main.cpp
+++ main.cpp
@@ -9,11 +9,44 @@
 #include "../ui.h"
 #include "SFML/Window/Keyboard.hpp"
 
-int windowWidth = 1600;
-int windowHeight = 900;
+uint windowWidth = 1600;
+uint windowHeight = 900;
+float aspectRatio;
 int resolutionWidth = 160;
+int resolutionHeight;
+float renderScale;
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode({800u, 600u}), "reymarc");
+    aspectRatio = (float) windowWidth / (float) windowHeight;
+    resolutionHeight = (float) resolutionWidth / aspectRatio;
+    renderScale = (float) windowWidth / (float) resolutionWidth;
+
+    sf::Image image(sf::Vector2u(resolutionWidth, resolutionHeight));
+    sf::Texture texture;
+    sf::Sprite sprite(texture);
+    sprite.setScale({renderScale, renderScale});
+    
+    sf::RenderWindow window(sf::VideoMode({windowWidth, windowHeight}), "reymarc");
+    while (window.isOpen()) {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>()) {
+                window.close();
+            }
+        }
+
+        for (unsigned x = 0; x < resolutionWidth; ++x) {
+            for (unsigned y = 0; y < resolutionHeight; ++y) {
+                image.setPixel({x, y}, sf::Color((x * 255)/resolutionWidth, (y * 255)/resolutionHeight, 0));
+            }
+        }
+        (void)texture.loadFromImage(image);
+        sprite = sf::Sprite(texture);
+        sprite.setScale({renderScale, renderScale});
+
+        window.clear();
+        window.draw(sprite);
+        window.display();
+    }
     return 0;
 }
